dca8807f90e773d7ee93c123621d8af5819ef81e
NgTuanAnh <tuananh1411201@gmail.com>
2025-02-27 17:12:06 +0700

    feat: initialize store for DetailProduct

--- src/shared/layout/Notfound/Notfound.tsx
+++ src/shared/layout/Notfound/Notfound.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./Notfound.scss";
 import { motion } from "framer-motion";
 import { btnAnimation } from "../../../common/constant/Constant";
+import React from "react";
 
 const Notfound = () => {
     return (
