207cea1b7d3abf6df45511331c090a53bfebc9ce
NgTuanAnh <tuananh1411201@gmail.com>
2025-03-31 17:47:39 +0700

    Feat: Setup initial UI components for ShopPage

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+import { useProductStore } from "../../common/hooks/useCustomHooks";
 
 const Shop = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const {listProducts} = useProductStore();
+
+
     return (
         <>
-            <div>shop page</div>
+            <section className="shop">
+                <Container>
+                    <Row>
+                        <Col>
+                            <p className="mb-0">Shop</p>
+                            <span>Showing 1-12 of 31 results</span>
+                        </Col>
+                    </Row>
+                    
+                    <Row>
+                        <Col className="custom-col side-bar">side bar</Col>
+                        <Col className="custom-col side-main">side main</Col>
+                    </Row>
+                </Container>
+            </section>
         </>
     );
 };
