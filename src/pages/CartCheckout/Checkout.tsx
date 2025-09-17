3343002a9f670c3a51a108733ece093c91c13985
NgTuanAnh <tuananh1411201@gmail.com>
2025-09-17 14:09:39 +0700

    feat(checkout): update logic and auto-fill form with API data

--- src/pages/CartCheckout/Checkout.tsx
+++ src/pages/CartCheckout/Checkout.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Col, Container, NavItem, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { yupFields } from "../../common/utils/Utils";
 import { useDispatch } from "react-redux";
